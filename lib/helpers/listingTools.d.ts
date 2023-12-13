eb983b90d11fe34d6f546fbcd40ded1a7e6ee6df
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-12-13 14:10:51 +0100

    publish 0.2.27

--- lib/helpers/listingTools.d.ts
+++ lib/helpers/listingTools.d.ts
@@ -30,6 +30,9 @@ declare const PREMIUM_LISTING: {
     tokenConditionalSwapMakerFeeRate: number;
     flashLoanSwapFeeRate: number;
     reduceOnly: number;
+    interestCurveScaling: number;
+    interestTargetUtilization: number;
+    depositLimit: number;
 };
 export type ListingPreset = typeof PREMIUM_LISTING;
 export type LISTING_PRESETS_KEYS = "ULTRA_PREMIUM" | "PREMIUM" | "MID" | "MEME" | "SHIT" | "UNTRUSTED";
